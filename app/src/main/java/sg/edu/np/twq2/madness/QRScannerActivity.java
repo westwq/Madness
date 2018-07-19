dd8edc7e5df30f9cd9f87fdc5b38005dec8e64d6
twq2 <twq2@np.edu.sg>
2018-07-19 18:23:52 +0800

    Added QR scanner (cant get scan result)

--- app/src/main/java/sg/edu/np/twq2/madness/QRScannerActivity.java
+++ app/src/main/java/sg/edu/np/twq2/madness/QRScannerActivity.java
@@ -3,11 +3,29 @@ package sg.edu.np.twq2.madness;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class QRScannerActivity extends AppCompatActivity {
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
+public class QRScannerActivity extends
+        /*AppCompatActivity {
+
+            private CaptureManager capture;
+            private DecoratedBarcodeView scanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
+
+        scanner = (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
+        capture = new CaptureManager(this, scanner);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+    }*/
+        CaptureActivity {
+    @Override
+    protected DecoratedBarcodeView initializeContent() {
+        setContentView(R.layout.activity_qrscanner);
+        return (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
     }
 }
