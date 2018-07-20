268013e8a20b304f3a926d89b98b3c4586f61325
westwq <31364147+westwq@users.noreply.github.com>
2018-07-20 23:25:24 +0800

    Renamed mainactivity to loginactivity

--- app/src/main/java/sg/edu/np/twq2/madness/LoginActivity.java
+++ app/src/main/java/sg/edu/np/twq2/madness/LoginActivity.java
@@ -18,9 +18,8 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = "GoogleActivity";
 
@@ -33,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // [START config_signin]
         // Configure Google Sign In
